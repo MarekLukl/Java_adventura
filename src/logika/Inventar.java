27c148f36c816d6171892bf65f5d66d80fddbae1
MarekLukl <98213675+MarekLukl@users.noreply.github.com>
2022-04-08 01:46:43 +0200

    automat na jídlo a automat na směnu ( stále chyba s inventar is null)

--- src/logika/Inventar.java
+++ src/logika/Inventar.java
@@ -19,23 +19,40 @@ public class Inventar {
     }
         return inventar;
     }
+    public Vec getVec(String nazev) {
+        if (inventar.containsKey(nazev)) {
+            return inventar.get(nazev);
+        }
+        return null;
+    }
     public static String getStavPenezenky(Inventar inventar){
         String stavPenezenky = "";
-        try {
-            inventar.getInventar();
-        }catch (IllegalArgumentException ex) {
-            System.out.println("Jsi švorc");
+        if (Inventar.inventar.isEmpty()||!(Inventar.inventar.containsKey("euro")) && !(Inventar.inventar.containsKey("rubly"))) {
+            return "Jsi švorc";
         }
 
-        for(String s: inventar.getInventar().keySet()){
-            if(inventar.getInventar().get(s).getNazev()=="euro"){
-                stavPenezenky += "euro" + "(" + inventar.getInventar().get(s).getMnozstvi() + ")" + "\n";
+        for(String s: Inventar.inventar.keySet()){
+            if(Inventar.inventar.get(s).getNazev() == "euro"){
+                stavPenezenky += "euro" + "(" + Inventar.inventar.get(s).getMnozstvi() + ")" + "\n";
             }
-            else if(inventar.getInventar().get(s).getNazev()=="rubly"){
-                stavPenezenky += "     " + inventar.getInventar().get(s).getMnozstvi() + "rublů";
+            else if(Inventar.inventar.get(s).getNazev() == "rubly"){
+                stavPenezenky += "     " + Inventar.inventar.get(s).getMnozstvi() + "rublů";
             }
         }
         return  stavPenezenky;
+    }
+    public static boolean nakup(int cena, String mena){
+        if(!(Inventar.inventar.containsKey(mena))){
+            System.out.println("Nemáš " + mena);
+            return false;
+        }
+        if(Inventar.inventar.get(mena).getMnozstvi() < cena){
+            System.out.println("Nemas dostatek penez");
+            return false;
+        }else{
+            Inventar.inventar.get(mena).setMnozstvi(Inventar.inventar.get(mena).getMnozstvi()-cena);
+            return true;
+        }
     }
 
 }
--- src/logika/PrikazPouzij.java
+++ src/logika/PrikazPouzij.java
@@ -25,7 +25,14 @@ public class PrikazPouzij implements IPrikaz{
         if(vec == null){
             return "Taková věc v této místnosti není." + "\n" + prostor.dlouhyPopis();
         } else if(vec.lzePouzit()){
-            uzitAutomatJidlo();
+
+            if(vec.getNazev()=="automat_na_jídlo"){
+                uzitAutomatJidlo();
+            }else if(vec.getNazev()=="exchange_automat"){
+                uzitExchangeAutomat();
+            }else if(vec.getNazev()=="gambling_automat"){
+
+            }
 
             return "Stav tvé peněženky: " + Inventar.getStavPenezenky(inventar);
         }
@@ -46,16 +53,100 @@ public class PrikazPouzij implements IPrikaz{
             vyber = sc.nextLine();
             if(vyber.equals("1")){
                 Vec bageta = new Vec("bageta",true,false,"jidlo");
+                if(inventar.nakup(5,"euro")){break;}
                 inventar.vlozitDoInvent(bageta.getNazev(),bageta);
+                System.out.println("Bageta koupena");
                 break;
             }else if(vyber.equals("2")){
                 Vec banan = new Vec("banán",true,false,"jidlo");
+                if(inventar.nakup(10,"euro")){break;}
                 inventar.vlozitDoInvent(banan.getNazev(),banan);
+                System.out.println("Banán koupen");
                 break;
-            }else{
+            }else if(vyber.equals("3")){
+                break;
+            } else{
                 System.out.println("Neplatný příkaz");
             }
         }
         System.out.println("Konec interakce s automatem");
+    }
+
+    public void uzitExchangeAutomat(){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        int pozadovanaCastka;
+        System.out.println("Jakou měnu si přejete?  pro eura napište '1'   pro rubly '2'    pro ukončení '3'");
+        while(!(input.equals("3"))){
+            input = sc.nextLine();
+            if(input.equals("1")){
+                pozadovanaCastka = pozadovanaCastka(1);
+                inventar.getVec("euro").setMnozstvi(inventar.getVec("euro")
+                        .getMnozstvi()+pozadovanaCastka); //zvyší počet euro o požadovanou částku
+                inventar.getVec("rubly").setMnozstvi(inventar.getVec("rubly")
+                        .getMnozstvi()-pozadovanaCastka); // odebere směnené rubly
+            }else if(input.equals("2")){
+                pozadovanaCastka = pozadovanaCastka(2);
+                inventar.getVec("rubly").setMnozstvi(inventar.getVec("rubly")
+                        .getMnozstvi()+ pozadovanaCastka); //zvyší počet rublů o požadovanou částku
+                inventar.getVec("euro").setMnozstvi(inventar.getVec("euro")
+                        .getMnozstvi()-pozadovanaCastka); // odebere směnené eura
+            }
+        }
+
+    }
+    public int pozadovanaCastka(int mena){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int pozadovanaCastka = 0;
+        boolean nesplneno = true;
+
+        if(mena ==1){
+            int maxEur = (int) Math.round((inventar.getVec("rubly").
+                    getMnozstvi())* 0.01); //vypočítaná a zaokrouhlená maximální hodnota vlastněných rublů v eurech
+            System.out.println("Kolik euro chceš? Maximálně: " + maxEur);
+            while(nesplneno){
+                input = sc.nextLine();
+                if(parseIntOrNull(input)==null){
+                    System.out.println("musíš zadat celočíselné číslo");
+                    continue;
+                }
+                pozadovanaCastka = parseIntOrNull(input);
+                if(pozadovanaCastka > maxEur){
+                    System.out.println("Na to nemáš. Maximálně: " + maxEur);
+                    continue;
+                }
+                else{
+                    nesplneno = false;
+                }
+            }
+        }
+        else if (mena == 2){
+            int maxRublu = (int) Math.round((inventar.getVec("euro").getMnozstvi())* 100);
+            System.out.println("Kolik euro chceš? Maximálně: " + maxRublu);
+            while(nesplneno){
+                input = sc.nextLine();
+                if(parseIntOrNull(input)==null){
+                    System.out.println("musíš zadat celočíselné číslo");
+                    continue;
+                }
+                pozadovanaCastka = parseIntOrNull(input);
+                if(pozadovanaCastka > maxRublu){
+                    System.out.println("Na to nemáš. Maximálně: " + maxRublu);
+                    continue;
+                }
+                else{
+                    nesplneno = false;
+                }
+            }
+        }
+        return pozadovanaCastka;
+    }
+    public Integer parseIntOrNull(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
